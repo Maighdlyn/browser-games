ae6cef220aa7f9a2debb81dbb5c7bcfa5358db94
Emma <maighdlyn@Astra.local>
2017-04-27 17:52:39 -0700

    Made Simon front end. Back end works in console. Haven't added flashing buttons yet.

--- simon.js
+++ simon.js
@@ -1,32 +1,34 @@
 $(document).ready(function () {
 
-    var colorArray = ["red", "yellow", "blue", "green"];
-    var randomSequence = [];
+	var colorArray = ["red", "yellow", "blue", "green"];
+	var randomSequence = [];
 
-    function addRandomColor() {
-        var randomNumber = Math.floor(Math.random() * 4);
-        randomSequence.push(colorArray[randomNumber]);
-        console.log("randomSequence = ", randomSequence);
-    };
+	function addRandomColor() {
+		var randomNumber = Math.floor(Math.random() * 4);
+		randomSequence.push(colorArray[randomNumber]);
+		console.log("randomSequence = ", randomSequence);
+	};
 
-    $('#start').click(function () {
-        addRandomColor();
-    });
+	$('#start').click(function () {
+		randomSequence = [];
+		userSequence = [];
+		addRandomColor();
+		document.querySelector('#screen').innerText = "- -"
+	});
 
-    var userSequence = [];
-    $('.colorButton').click(function () {
-        var colorClicked = $(this).attr('id');
-        userSequence.push(colorClicked);
-        console.log("userSequence = ", userSequence);
-        if (userSequence.length === randomSequence.length) {
-            if (randomSequence.value === userSequence.value) {
-                console.log("That's right!");
-                addRandomColor();
-                userSequence = [];
-            };
-        };
-
-
-    });
-
+	var userSequence = [];
+	$('.colorButton').click(function () {
+		var colorClicked = $(this).attr('id');
+		userSequence.push(colorClicked);
+		console.log("userSequence = ", userSequence);
+		if (userSequence.length === randomSequence.length) {
+			if (randomSequence.toString() === userSequence.toString()) {
+				document.querySelector('#screen').innerText = "GOOD JOB";
+				addRandomColor();
+				userSequence = [];
+			} else {
+				document.querySelector('#screen').innerText = "GAME OVER";
+			};
+		};
+	});
 });
--- ticTacToe.js
+++ ticTacToe.js
@@ -1,80 +1,80 @@
 $(document).ready(function () {
-    function showMessage(messageText) {
-        var messageElem = document.querySelector('#textBox');
-        messageElem.innerText = messageText;
-    }
+	function showMessage(messageText) {
+		var messageElem = document.querySelector('#textBox');
+		messageElem.innerText = messageText;
+	}
 
-    function check() {
-        var space0 = document.getElementById('space0').textContent;
-        var space1 = document.getElementById('space1').textContent;
-        var space2 = document.getElementById('space2').textContent;
-        var space3 = document.getElementById('space3').textContent;
-        var space4 = document.getElementById('space4').textContent;
-        var space5 = document.getElementById('space5').textContent;
-        var space6 = document.getElementById('space6').textContent;
-        var space7 = document.getElementById('space7').textContent;
-        var space8 = document.getElementById('space8').textContent;
+	function check() {
+		var space0 = document.getElementById('space0').textContent;
+		var space1 = document.getElementById('space1').textContent;
+		var space2 = document.getElementById('space2').textContent;
+		var space3 = document.getElementById('space3').textContent;
+		var space4 = document.getElementById('space4').textContent;
+		var space5 = document.getElementById('space5').textContent;
+		var space6 = document.getElementById('space6').textContent;
+		var space7 = document.getElementById('space7').textContent;
+		var space8 = document.getElementById('space8').textContent;
 
-        if (space0 === space1 && space1 === space2 && space0 != "") {
-            showMessage("You Win! Top Across!");
-            
-        } else if (space3 === space4 && space4 === space5 && space3 != "") {
-            showMessage("You Win! Middle Across!");
-        } else if (space6 === space7 && space7 === space8 && space6 != "") {
-            showMessage("You Win! Bottom Across!");
-        } else if (space0 === space3 && space3 === space6 && space3 != "") {
-            showMessage("You Win! Left Down!");
-        } else if (space1 === space4 && space4 === space7 && space1 != "") {
-            showMessage("You Win! Middle Down!");
-        } else if (space2 === space5 && space5 === space8 && space5 != "") {
-            showMessage("You Win! Right Down!");
-        } else if (space0 === space4 && space4 === space8 && space0 != "") {
-            showMessage("You Win! Diagonal!");
-        } else if (space2 === space4 && space4 === space6 && space2 != "") {
-            showMessage("You Win! Diagonal!");
-        };
-    };
+		if (space0 === space1 && space1 === space2 && space0 != "") {
+			showMessage("You Win! Top Across!");
 
-    $('#twoPlayer').click(function () {
-        $(this).addClass('highlight');
-        var clickCounter = 0;
-        $('.XO').click(function () {
-            if (this.textContent === "") {
-                clickCounter += 1;
-                if (clickCounter % 2 == 0) {
-                    this.innerText = "O";
-                } else {
-                    this.innerText = "X";
-                }
-            };
-            check();
-        });
-    });
+		} else if (space3 === space4 && space4 === space5 && space3 != "") {
+			showMessage("You Win! Middle Across!");
+		} else if (space6 === space7 && space7 === space8 && space6 != "") {
+			showMessage("You Win! Bottom Across!");
+		} else if (space0 === space3 && space3 === space6 && space3 != "") {
+			showMessage("You Win! Left Down!");
+		} else if (space1 === space4 && space4 === space7 && space1 != "") {
+			showMessage("You Win! Middle Down!");
+		} else if (space2 === space5 && space5 === space8 && space5 != "") {
+			showMessage("You Win! Right Down!");
+		} else if (space0 === space4 && space4 === space8 && space0 != "") {
+			showMessage("You Win! Diagonal!");
+		} else if (space2 === space4 && space4 === space6 && space2 != "") {
+			showMessage("You Win! Diagonal!");
+		};
+	};
 
-    $('#onePlayer').click(function () {
-        $('.XO').click(function () {
-            if (this.textContent === "") {
-                this.innerHTML = "X";
-            };
+	$('#twoPlayer').click(function () {
+		$(this).addClass('highlight');
+		var clickCounter = 0;
+		$('.XO').click(function () {
+			if (this.textContent === "") {
+				clickCounter += 1;
+				if (clickCounter % 2 == 0) {
+					this.innerText = "O";
+				} else {
+					this.innerText = "X";
+				}
+			};
+			check();
+		});
+	});
 
-            var spaceArray = ["space0", "space1", "space2", "space3", "space4", "space5", "space6", "space7", "space8"];
-            var emptySpacesArray = [];
-            for (var i = 0; i <= 8; i++) {
-                if (document.getElementById(spaceArray[i]).textContent === "") {
-                    emptySpacesArray.push(spaceArray[i]);
-                };
-            }
+	$('#onePlayer').click(function () {
+		$('.XO').click(function () {
+			if (this.textContent === "") {
+				this.innerHTML = "X";
+			};
 
-            var randomNumber = Math.floor(Math.random() * emptySpacesArray.length);
-            var randomSpace = document.getElementById(emptySpacesArray[randomNumber]);
-            randomSpace.innerText = "O";
+			var spaceArray = ["space0", "space1", "space2", "space3", "space4", "space5", "space6", "space7", "space8"];
+			var emptySpacesArray = [];
+			for (var i = 0; i <= 8; i++) {
+				if (document.getElementById(spaceArray[i]).textContent === "") {
+					emptySpacesArray.push(spaceArray[i]);
+				};
+			}
 
-            check();
-        });
+			var randomNumber = Math.floor(Math.random() * emptySpacesArray.length);
+			var randomSpace = document.getElementById(emptySpacesArray[randomNumber]);
+			randomSpace.innerText = "O";
 
-    });
+			check();
+		});
 
-    $('#newGame').click(function () {
-        location.reload();
-    });
+	});
+
+	$('#newGame').click(function () {
+		location.reload();
+	});
 });
